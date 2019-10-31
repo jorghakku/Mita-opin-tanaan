7e3f4e0676ae08d5642c0100f61aa8dda1dc0231
jorghakku <57213434+jorghakku@users.noreply.github.com>
2019-10-31 12:28:26 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="fi">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Harjoitus 1</title>
+    <style>
+        body {
+            width: 600px;
+            margin: 0 auto;
+            background-color: #eee;
+            font-family: sans-serif;
+        }
+        .korostettu {
+            border: 1px solid white;
+            padding: 2em;
+            text-align: center;
+        }
+
+    </style>
+</head>
+<body>
+    <h1>Mitä opin tänään</h1>
+    <p>Tervetuloa</p>
+    
+    
+    <form action="lisaa oivallus.php" methdod="post">
+        <input type="text" name="lisaa uusi oivallus"></input>
+        <input type="submit" value="Lisää uusi"
+        </form><br>
+
+        <table>
+            <tr>
+                <th>Päivä</th>
+                <th>Oivallus</th>
+                <th>Poista</th
+            </tr
+            <tr>
+                <td>31.10.2019</td>
+                <td>Oivallus</td>
+                <td>X</td>
+            </tr>
+        </table>
+
+        <label for="fname">Pvm</label>
+        <input type="text" id="" name="" placeholder="">
+      
+        <label for="lname">Oivallettu asia</label>
+        <input type="text" id="" name="" placeholder="">
+      
+        <input type="submit" value="Poista"><br>
+      
+        <label for="fname">Pvm</label>
+        <input type="text" id="" name="" placeholder="">
+      
+        <label for="lname">Oivallettu asia</label>
+        <input type="text" id="" name="" placeholder="">
+      
+        <input type="submit" value="Poista"><br>
+
+        <label for="fname">Pvm</label>
+        <input type="text" id="" name="" placeholder="">
+      
+        <label for="lname">Oivallettu asia</label>
+        <input type="text" id="" name="" placeholder="">
+      
+        <input type="submit" value="Poista"
+</body>
+</html>
